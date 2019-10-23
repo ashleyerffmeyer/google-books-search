85a21e095ac44417bb1d8352718de4ed4d2eca06
ashleyerffmeyer <ashley.erffmeyer@gmail.com>
2019-10-23 13:47:42 -0500

    updated button styling

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="main">
 
         {/* Navigation */}
         <Nav />
